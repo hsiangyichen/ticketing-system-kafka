d9cb45e8cfbde306db7fdcaaaad5204ac47a2e54
hsiangyichen <hsiangyi1025@gmail.com>
2025-04-27 14:39:39 -0700

    feat: add Kafka consumer connection and message handling

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { connectKafka } from "./services/kafkaClient.js";
 
 const app = express();
 const PORT = 8081;
+
+// Connect to Kafka consumer
+connectKafka().catch(console.error);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Ticketing Service!");
